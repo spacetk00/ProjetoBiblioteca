10f055383e0ce6751e8f26fccd07c1e2b81831ef
Paulo <paulorcmv@gmail.com>
2021-08-05 16:01:00 -0300

    Modificação do login com uso de registros do banco de dados

--- Controllers/Autenticacao.cs
+++ Controllers/Autenticacao.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Biblioteca.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 
 namespace Biblioteca.Controllers
@@ -8,9 +11,34 @@ namespace Biblioteca.Controllers
     {
         public static void CheckLogin(Controller controller)
         {   
-            if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("user")))
+            if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("Login")))
             {
                 controller.Request.HttpContext.Response.Redirect("/Home/Login");
+            }
+        }
+
+        public static bool verificaLoginSenha(string login, string senha, Controller controller)
+        {
+            using(BibliotecaContext bc = new BibliotecaContext())
+            {
+                //verificaSeUsuarioAdminExiste(bc);
+
+                //senha = Criptografo.TextoCriptogtrafado(senha);
+
+                IQueryable<Usuario> UsuarioEncontrado = bc.Usuarios.Where(u => u.Login==login && u.Senha==senha);
+                List<Usuario> ListarUsuarioEcontrado = UsuarioEncontrado.ToList();
+
+                if(ListarUsuarioEcontrado.Count==0)
+                {
+                    return false;
+                }
+                else
+                {
+                    controller.HttpContext.Session.SetString("Login", ListarUsuarioEcontrado[0].Login);
+                    controller.HttpContext.Session.SetString("Nome", ListarUsuarioEcontrado[0].Nome);
+                    controller.HttpContext.Session.SetInt32("Tipo", ListarUsuarioEcontrado[0].Tipo);
+                    return true;
+                }
             }
         }
     }
